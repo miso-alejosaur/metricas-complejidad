19a816f7f94554516f3a5a72a2a7d174dbe885fc
Alejandro Santamaria <a.santamaria11@uniandes.edu.co>
2023-11-21 22:10:04 -0500

    Changes

--- src/shipping/Shipping.java
+++ src/shipping/Shipping.java
@@ -1,59 +1,69 @@
 package shipping;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Shipping {
-	
+	public static Map<String, Integer> multiplicadores = new HashMap<>();
+	public static Integer precioBase = 100;
+	public static Integer pesoTope = 5;
+	public static Integer precioAdicional = 10;
+
+	static {
+		multiplicadores.put("argentina", 1);
+		multiplicadores.put("brasil", 2);
+		multiplicadores.put("chile", 3);
+		multiplicadores.put("uruguay", 4);
+	}
+
 	public Shipping() {
 		super();
 	}
-	
+
 	public int calcularCostoEnvio(String pais, int peso) {
-		int costo = -1;
-		if(pais.equalsIgnoreCase("Argentina")) {
-			if(peso <= 5)
-				costo = 100;
-			else
-				costo = 100 + (peso - 5) * 10;
-		} else if(pais.equalsIgnoreCase("Brasil")) {
-			if(peso <= 5) {
-				costo = 200;
-			} else {
-				costo = 200 + (peso - 5) * 20;
-			}
-		} else if(pais.equalsIgnoreCase("Chile")) {
-			if(peso <= 5) {
-				costo = 300;
-			} else {
-				costo = 300 + (peso - 5) * 30;
-			}
-		} else if(pais.equalsIgnoreCase("Uruguay")) {
-			if(peso <= 5) 
-				costo = 400;
-			else 
-				costo = 400 + (peso - 5) * 40;
-		}
-		
+		String paisMinusculas = pais.toLowerCase();
+		int costo = obtenerCosto(peso, paisMinusculas);
+
 		return costo;
 	}
-	
+
+	private int obtenerCosto(int peso, String paisMinusculas) {
+		if (!multiplicadores.containsKey(paisMinusculas))
+			return -1;
+			
+		int multiplicador = multiplicadores.get(paisMinusculas);
+
+		int costo = precioBase * multiplicador;
+		
+		if (peso > pesoTope)
+			costo += (peso - pesoTope) * 10 * multiplicador;
+
+		return costo;
+	}
+
 	public static void main(String[] args) {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			int peso = Integer.parseInt(in.readLine());
 			String pais = in.readLine();
-			
+
 			Shipping ship = new Shipping();
 			int costo = ship.calcularCostoEnvio(pais, peso);
-			if(costo == -1) {
+
+			mostrarCosto(costo);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static void mostrarCosto(int costo) {
+			if (costo == -1) {
 				System.out.println("El pais ingresado no es valido");
 			} else {
 				System.out.println("El costo del envio es: " + costo);
 			}
-		} catch(IOException e) {
-			System.out.println(e.getMessage());
-		}
-		
 	}
- }
+}
